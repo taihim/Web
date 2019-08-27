f0d46a36c6369dd312951d6e6d860a2efdbb7afa
Tigrex161 <tai.him18@gmail.com>
2019-08-27 21:49:53 +0500

    Responsive Design

--- Portfolio Page/dist/JS/main.js
+++ Portfolio Page/dist/JS/main.js
@@ -19,25 +19,12 @@ menuBtn.addEventListener('click', toggleMenu);
 
 function toggleMenu()
 {
-    if(!showMenu)
-    {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuBranding.classList.add('show');
-        menuNav.classList.add('show');
-        navItems.forEach(item => item.classList.add(show));
+        menuBtn.classList.toggle('close');
+        menu.classList.toggle('show');
+        menuBranding.classList.toggle('show');
+        menuNav.classList.toggle('show');
+        navItems.forEach(item => item.classList.toggle('show'));
     
-        showMenu = true;
-    }
-    else
-    {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuBranding.classList.remove('show');
-        menuNav.classList.remove('show');
-        navItems.forEach(item => item.classList.remove(show));
-
-        showMenu = false;
-
-    }
+        showMenu = !showMenu;
+       
 }
